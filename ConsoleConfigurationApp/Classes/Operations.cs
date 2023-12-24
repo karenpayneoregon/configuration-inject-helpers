8898551795d7be189322a196a73f6781a46734d7
Karen Payne <paynekaren@comcast.net>
2023-12-24 04:02:15 -0800

    minor change to examples

--- ConsoleConfigurationApp/Classes/Operations.cs
+++ ConsoleConfigurationApp/Classes/Operations.cs
@@ -1,20 +1,20 @@
 ﻿using ConsoleConfigurationApp.Models;
-using ConsoleConfigurationLibrary.Classes;
 using Dapper;
 using Microsoft.Data.SqlClient;
+using static ConsoleConfigurationLibrary.Classes.AppConnections;
 
 namespace ConsoleConfigurationApp.Classes;
 internal class Operations
 {
     public static List<Track> TrackList(int albumId)
     {
-        using SqlConnection cn = new(AppConnections.Instance.MainConnection);
+        using SqlConnection cn = new(Instance.MainConnection);
         return cn.Query<Track>(SqlStatements.TracksByAlbumId, new { AlbumId = albumId }).ToList();
     }
 
     public static List<Categories> CategoryList()
     {
-        using SqlConnection cn = new(AppConnections.Instance.SecondaryConnection);
+        using SqlConnection cn = new(Instance.SecondaryConnection);
         return cn.Query<Categories>(SqlStatements.Categories).ToList();
     }
 }
