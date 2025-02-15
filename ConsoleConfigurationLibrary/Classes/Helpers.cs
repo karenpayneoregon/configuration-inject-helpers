c3cdd932ae9faf26b3eaced929aeb3c98fd0f490
Karen Payne <paynekaren@comcast.net>
2025-02-15 14:52:14 -0800

    Bump version to 1.0.0.7 and add Helpers class
    Updated `ConsoleConfigurationLibrary.csproj` to change the assembly, file, and version numbers to `1.0.0.7`.
    Introduced a new `Helpers` class in `Helpers.cs` with a method `SectionExists` to check for the existence of configuration sections.
    
    by Karen

---
+++ ConsoleConfigurationLibrary/Classes/Helpers.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConsoleConfigurationLibrary.Classes
+{
+    public class Helpers
+    {
+        /// <summary>
+        /// Checks whether a configuration section with the specified name exists in the application's configuration.
+        /// </summary>
+        /// <param name="sectionName">The name of the configuration section to check for existence.</param>
+        /// <returns>
+        /// <c>true</c> if the configuration section exists; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool SectionExists(string sectionName) 
+            => Configuration.JsonRoot().GetSection(sectionName).Exists();
+    }
+}
